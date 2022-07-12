7d1b4792b53bd20956cba9c23ba80b5305002832
acin <mukhlishinmarsyad@students.amikom.ac.id>
2022-07-13 00:50:40 +0700

    Fix -jawa

--- js/konten.js
+++ js/konten.js
@@ -1,6 +1,8 @@
 const mapJawa = document.getElementById("jawa")
 const mapSumatra = document.getElementById("sumatra")
 const mapKalimantan = document.getElementById("kalimantan")
+const mapSulawesi = document.getElementById("sulawesi")
+const mapBali = document.getElementById("bali")
 
 function resetKartu() {
     const arrayKartu = document.querySelectorAll(".kartu")
@@ -26,4 +28,25 @@ mapSumatra.addEventListener("mouseenter", function(){
     const kartuSumatra = document.getElementById("kartuSumatra")
     mapSumatra.classList.toggle("map-active")
     setTimeout(1500,kartuSumatra.classList.toggle("kartu-show"))
+})
+
+mapKalimantan.addEventListener("mouseenter", function(){
+    resetKartu()
+    const kartuKalimantan = document.getElementById("kartuKalimantan")
+    mapKalimantan.classList.toggle("map-active")
+    setTimeout(1500,kartuKalimantan.classList.toggle("kartu-show"))
+})
+
+mapSulawesi.addEventListener("mouseenter", function(){
+    resetKartu()
+    const kartuSulawesi = document.getElementById("kartuSulawesi")
+    mapSulawesi.classList.toggle("map-active")
+    setTimeout(1500,kartuSulawesi.classList.toggle("kartu-show"))
+})
+
+mapBali.addEventListener("mouseenter", function(){
+    resetKartu()
+    const kartuBali = document.getElementById("kartuBali")
+    mapBali.classList.toggle("map-active")
+    setTimeout(1500,kartuBali.classList.toggle("kartu-show"))
 })
